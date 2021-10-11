0ae8b7abbb5011dbbe1351e7badf88fb9bc5f355
Mark Momot <markmomot03@gmail.com>
2021-10-11 17:28:32 -0400

    commit tree 05ba2d508f03069d7a6aa61958af288954424e8b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const headquarters = 42;
+
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(headquarters - blocks)
+}
+
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(end - start) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if(distance < 400){
+        return 0;
+    } else if(400 <= distance && distance < 2000){
+        return (distance-400)*.02;
+    } else if(2000 <= distance && distance < 2500){
+        return 25;
+    } else{
+        return 'cannot travel that far';
+    }
+}
